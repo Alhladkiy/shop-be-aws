13bca76f22604975584c33ec4ee20e49f094fc8f
Alhladkiy <Alhladkiy@gmail.com>
2022-06-14 21:44:25 +0300

    feat: update img products

--- product-service/mock/products.js
+++ product-service/mock/products.js
@@ -3,31 +3,31 @@ const products = [
     "count": 4,
     "description": "professional boots",
     "id": "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
-    "price": 240.00,
+    "price": 100,
     "title": "Nike Phantom GT2 Elite FG",
-    "img": "https://soccershop.by/image/cache/catalog/Footwear/Nike/new/FirmGround/DA4432-167_2-1024x1024.jpg"
+    "img": "https://soccershop.by/image/cache/catalog/Footwear/Nike/new/FirmGround/data-images-butsi-nike-db5611-611-693x465-1024x1024.jpg"
   },
   {
     "count": 6,
     "description": "professional boots",
     "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a0",
-    "price": 600.000,
+    "price": 195,
     "title": "Nike Mercurial Superfly 8",
-    "img": "https://soccershop.by/image/cache/catalog/Footwear/Nike/new/FirmGround/cv0958-760_1-1024x1024.jpg"
+    "img": "https://soccershop.by/image/cache/catalog/Footwear/Nike/FG/new/data-images-butsi-nike-cq7635-600-693x465-1024x1024.jpg"
   },
   {
     "count": 7,
     "description": "semi-professional boots",
     "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a2",
-    "price": 230.00,
+    "price": 205,
     "title": "Nike Phantom GT2 Elite FG",
-    "img": "https://soccershop.by/image/cache/catalog/Footwear/Nike/new/FirmGround/cz9890-004_g01-1024x1024.jpg"
+    "img": "https://soccershop.by/image/cache/catalog/Footwear/Nike/FG/new/data-images-butsi-nike-cq7635-090-693x465-1024x1024.jpg"
   },
   { 
     "count": 12,
     "description": "professional boots",
     "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
-    "price": 260.00,
+    "price": 260,
     "title": "Nike Phantom GT Elite FG",
     "img": "https://soccershop.by/image/cache/catalog/Footwear/Nike/new/FirmGround/unnamed-1024x1024.jpg"
   },
@@ -35,7 +35,7 @@ const products = [
     "count": 7,
     "description": "semi-professional boots",
     "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
-    "price": 360.00,
+    "price": 315,
     "title": "Nike Mercurial Vapor 14",
     "img": "https://soccershop.by/image/cache/catalog/Footwear/Nike/new/FirmGround/unnamed31414-1024x1024.jpg"
   },
@@ -43,23 +43,23 @@ const products = [
     "count": 8,
     "description": "professional boots",
     "id": "7567ec4b-b10c-48c5-9345-fc73348a80a1",
-    "price": 340.00,
+    "price": 340,
     "title": "Nike Mercurial Vapor 14",
-    "img": "https://soccershop.by/image/cache/catalog/Footwear/Nike/new/FirmGround/f6h-jjEaqk8-1024x1024.jpg"
+    "img": "https://soccershop.by/image/cache/catalog/Footwear/Nike/FG/new/data-images-butsi-nike-aq4176-060-693x465-1024x1024.jpg"
   },
   {
     "count": 2,
     "description": "semi-professional boots",
     "id": "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
-    "price": 230.00,
+    "price": 230,
     "title": "Nike Mercurial Vapor 14",
-    "img": "https://soccershop.by/image/cache/catalog/Footwear/Nike/new/FirmGround/CQ7635-105-1024x1024.jpg"
+    "img": "https://soccershop.by/image/cache/catalog/Footwear/Nike/FG/new/data-images-butsi-nike-aq4176-801-693x465-1024x1024.jpg"
   },
   {
     "count": 3,
     "description": "semi-professional boots",
     "id": "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
-    "price": 395.00,
+    "price": 600,
     "title": "Nike Phantom GT Elite SE",
     "img": "https://soccershop.by/image/cache/catalog/Footwear/Nike/new/FirmGround/data-images-butsi-nike-ct2156-001-693x465-1024x1024.jpg"
   }
