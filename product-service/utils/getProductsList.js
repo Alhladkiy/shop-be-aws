6765ebd19da466963098a7107b9c5bef433055e7
Alhladkiy <Alhladkiy@gmail.com>
2022-06-29 01:07:36 +0300

    refactor: rename function

--- product-service/utils/getProductsList.js
+++ product-service/utils/getProductsList.js
@@ -2,25 +2,25 @@ const { Client } = require('pg');
 
 const { dbOptions } = require('../helpers/dbOptions');
 
-const getProducts = (products = {}, status = 200) => (
+const handlerProductsList = (products = {}, status = 200) => (
   {
     headers: {
       "Access-Control-Allow-Headers" : "Content-Type",
       "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "OPTIONS, POST, GET"
+      "Access-Control-Allow-Methods": "GET"
     },
     statusCode: status,
     body: JSON.stringify(products),
   }
 );
     
-const handlerProductsList = async (event) => {
+const getProductsList = async (event) => {
   const client = new Client(dbOptions);
   await client.connect();
 
   try {
     const { rows: product } = await client.query(`select * from products`);
-    return await getProducts(product, 200);
+    return handlerProductsList(product, 200);
 
   } catch (err) {
     console.error('Error: ', err);
@@ -31,7 +31,7 @@ const handlerProductsList = async (event) => {
 }
 
 module.exports = {
-  handlerProductsList
+  getProductsList
 }
 
 
