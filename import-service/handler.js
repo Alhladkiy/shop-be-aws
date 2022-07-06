6e85162bad70bb31cd92da818a8a7cb8c2390245
Alhladkiy <Alhladkiy@gmail.com>
2022-07-07 01:26:53 +0300

    feat: update handler for import-service

--- import-service/handler.js
+++ import-service/handler.js
@@ -1,100 +1,122 @@
 const AWS = require('aws-sdk');
 const csvParser = require('csv-parser');
-const BUCKET = 'arn:aws:s3:::uploaded-alhladkiy';
 
-const importProductsFile = async function(event) {
-    const s3 = new AWS.S3({ region: 'eu-central-1' });
-    let statusCode = 200;
-    let body = {};
-    const file = event.queryStringParameters.name;
-    const catalog = `uploaded/${file}`;
+const BUCKET = 'uploaded-alhladkiy';
 
-    const params = {
-      Bucket: BUCKET,
-      Key: catalog,
-      Expires: 60,
-      ContentType: 'text/csv',
+const importProductsFile = async (event) => {
+  const s3 = new AWS.S3({ region: 'eu-central-1' });
+  
+  const { name } = event.queryStringParameters;
+
+  const catalogPath = `uploaded/${name}`;
+
+  const params = {
+    Bucket: BUCKET,
+    Key: catalogPath,
+    Expires: 60,
+    ContentType: 'text/csv',
+  };
+
+  try {
+    const signedURL = await new Promise((resolve, reject) => {
+      return  s3.getSignedUrl('putObject', params, (error, url) => {
+        console.log(error, url)
+        if (error || !url) {
+          reject(error);
+        }
+        resolve(url);
+      });
+    });
+    
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Headers" : "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET"
+      },
+      body: JSON.stringify({
+        url: signedURL
+      })
     };
 
-    try {
-      const signedURL = await new Promise((reject, resolve) => {
-        return  s3.getSignedUrl('putObject', params, (err, url) => {
-          if (err || !url) {
-            console.log('You have error!!!');
+  } catch (error) {
+    console.error('Error: ', error);
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify({
+        message: `Invalid error sorry`,
+      }),
+    };
+  }
+}
+
+const importFileParser = async (event) => {
+  const s3 = new AWS.S3({ region: 'eu-central-1' });
+
+  try {
+    for (const record of event.Records) {
+      const key = record.s3.object.key;
+
+      const params = {
+        Bucket: BUCKET,
+        Key: key,
+      };
+      await new Promise((resolve, reject) => {
+        const s3Stream = s3.getObject(params).createReadStream();
+        s3Stream
+          .pipe(csvParser())
+          .on('data', (data) => {
+            console.log(data);
+          })
+          .on('error', (err) => {
             reject(err);
-          }
-          resolve(url);
+          })
+        .on('end', async () => {
+          console.log(`Copying was successful`);
+
+          await s3.copyObject({
+            Bucket: BUCKET,
+            CopySource: `${BUCKET}/${key}`,
+            Key: key.replace('uploaded', 'parsed_file')
+          }).promise();
+
+          await s3.deleteObject({
+            Bucket: BUCKET,
+            Key: key
+          }).promise();
+          console.log('file delete')
+
+          resolve();
         });
       });
-        body = JSON.stringify({signedURL});
-
-    } catch (error) {
-      console.error('Error: ', error);
-      statusCode = 500;
-      body = error;
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify(data),
+      };
     }
-
+  } catch (error) {
+    console.error('Error: ', error);
     return {
-      statusCode,
-      headers: { 'Access-Control-Allow-Origin': '*' },
-      body,
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify({
+        message: "Error!!!!",
+      }),
     };
   }
-
-  const importFileParser = async function(event) {
-    const s3 = new AWS.S3({ region: 'eu-west-1' });
-    let statusCode = 202;
-    let body = {};
-
-    for (const record of event.Records) {
-
-      try {
-        const params = {
-          Bucket: BUCKET,
-          Key: record.s3.object.key,
-        };
-        const s3Stream = s3.getObject(params).createReadStream();
-        await new Promise((resolve, reject) => {
-          s3Stream
-            .pipe(csvParser())
-            .on('data', (chunk) => {
-              console.log(chunk);
-            })
-            .on('error', (err) => {
-              reject(err);
-            })
-          .on('end', async () => {
-            console.log(`Copy in ${BUCKET}/${record.s3.object.key.replace('uploaded', 'parsed')}`);
-
-            await s3.copyObject({
-              Bucket: BUCKET,
-              CopySource: BUCKET + '/' + record.s3.object.key,
-              Key: record.s3.object.key.replace('uploaded', 'parsed')
-            }).promise();
-
-            await s3.deleteObject({
-              Bucket: BUCKET,
-              Key: record.s3.object.key
-            }).promise();
-
-            resolve(() => {});
-          });
-        });
-
-      } catch (error) {
-        console.error('Your err:', error);
-        statusCode = 500;
-        body = error;
-      }
-    }
-
-    return {
-      statusCode,
-      headers: { 'Access-Control-Allow-Origin': '*' },
-      body,
-    }
-  }
-
+}
 
   module.exports = {
     importProductsFile,
