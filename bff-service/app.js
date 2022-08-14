d382e08d60df03a5eac0d75aeb48c59c1b973921
Alhladkiy <Alhladkiy@gmail.com>
2022-08-14 16:09:45 +0300

    feat: add express app

--- bff-service/app.js
+++ bff-service/app.js
@@ -1,8 +1,58 @@
-import express from 'express';
+const express = require('express');
+const dotenv = require('dotenv');
+require('dotenv').config();
+const axios = require('axios').default;
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log("Server start...")
+app.use(express.json());
+
+app.all('/*', (req, res) => {
+    console.log('originalUrl', req.originalUrl);
+    console.log('method', req.method);
+    console.log('body', req.body);
+    
+    const recipient = req.originalUrl.split('/')[1];
+    console.log('recipient', recipient);
+
+    const recipientURL = process.env[recipient];
+    console.log('recipientURL', recipientURL);
+
+
+    if(recipientURL) {
+        const axiosConfig = {
+            method: req.method,
+            url: `${recipientURL}${req.originalUrl}`,
+            ...(Object.keys(req.body || {}).length > 0 && {data: req.body}),
+
+        };
+        console.log('axiosConfig', axiosConfig);
+
+        axios(axiosConfig)
+        .then(function(response) {
+            console.log('response from recipient', response.data);
+            res.json(response.data);
+        })
+        .catch((err) => {
+            console.log('some err: ', JSON.stringify(err));
+
+            if (err.response) {
+                const {
+                    status,
+                    data
+                } = err.response;
+
+                res.status(status).json(data);
+            } else {
+                res.status(500).json({err: err.message});
+            }
+        })
+    } else {
+        res.status(502).json({err: 'Cannot process request'});
+    }
+});
+
+app.listen(PORT, (err) => {
+    console.log(`BFF APP started on port ${PORT}`)
 })
